a614e558530f329cc1f6d898a156377d231dc7b9
Abuka-Victor <abukavictoro@gmail.com>
2022-09-17 01:06:58 +0100

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,4 @@
 # A puppet file to kill a process calles killmenow
 exec { 'pkill -9 killmenow'
-  path    => 'usr/local/bin:/usr/bin:/usr/sbin:/bin'
-  returns => '0',
+  path    => 'usr/local/bin:/usr/bin:/usr/sbin:/bin',
 }
